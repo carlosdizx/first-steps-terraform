1d50be111e8403999fed09250045e68454464ab2
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-22 13:03:07 -0500

    :sparkles: desacoplate variable

--- ex7/ext7_inputs.tf
+++ ex7/ext7_inputs.tf
@@ -14,12 +14,3 @@ resource "aws_vpc" "vpc_ohio" {
   }
   provider = aws.ohio
 }
-
-# This in other file
-variable "virginia_cidr" {
-  default = "10.10.0.0/16"
-}
-
-variable "ohio_cidr" {
-  default = "10.20.0.0/16"
-}
---
+++ ex7/variables.tf
@@ -0,0 +1,7 @@
+variable "virginia_cidr" {
+  default = "10.10.0.0/16"
+}
+
+variable "ohio_cidr" {
+  default = "10.20.0.0/16"
+}
