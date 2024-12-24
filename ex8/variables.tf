d76878ab819ea84f81e5831a97bea01bfee4c7a0
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-24 12:33:29 -0500

    :sparkles: add variables

---
+++ ex8/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_main_cidr" {
+  description = "Dirección ip de la vpc"
+  type        = string
+}
+
+variable "subnet_main_public_cidr" {
+  description = "Dirección ip publica de la subnet"
+  type        = string
+}
+
+variable "subnet_main_private_cidr" {
+  description = "Dirección ip publica de la subnet"
+  type        = string
+}
