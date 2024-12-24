339001213132801239551164ac4031d9f8bf7685
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-24 16:35:35 -0500

    :sparkles: add tag

--- ex8/ext8_ec2.tf
+++ ex8/ext8_ec2.tf
@@ -2,4 +2,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-01816d07b1128cd2d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.vpc_main_public_subnet.id
+  tags = {
+    Name : "Wer Server 1"
+  }
 }
