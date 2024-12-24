a9746ec9801887d9a47ed6919cf85eb938af330f
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-24 12:59:51 -0500

    :sparkles: set default tags and set specific tag for resource

--- ex8/ext8.tf
+++ ex8/ext8.tf
@@ -1,8 +1,7 @@
 resource "aws_vpc" "vpc_main" {
   cidr_block = var.vpc_main_cidr
   tags = {
-    Name = "VPC_VIRGINIA",
-    env  = "Dev"
+    Name = "Vpc Main",
   }
 }
 
@@ -10,9 +9,15 @@ resource "aws_subnet" "vpc_main_public_subnet" {
   vpc_id                  = aws_vpc.vpc_main.id
   cidr_block              = var.subnets_main_cidr[0]
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Public subnet",
+  }
 }
 
 resource "aws_subnet" "vpc_main_private_subnet" {
   vpc_id     = aws_vpc.vpc_main.id
   cidr_block = var.subnets_main_cidr[1]
+  tags = {
+    Name = "Private subnet",
+  }
 }
