eb73369c0ed502081d6e73bad1cb79f90c7391d0
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-25 12:30:17 -0500

    :sparkles: create a data source for manage existing resources in aws

--- ex8/ext8_ec2.tf
+++ ex8/ext8_ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-01816d07b1128cd2d"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
+  key_name      = data.aws_key_pair.myKey.key_name
   tags = {
     Name : "Web Server 1"
   }
