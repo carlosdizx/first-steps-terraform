adad2b1fc4a2fadb667f8b280394e843f2da93c6
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-26 23:06:43 -0500

    :sparkles: configure internet gateway and security group for instance

--- ex11/ex11_vpc.tf
+++ ex11/ex11_vpc.tf
@@ -49,4 +49,28 @@ resource "aws_route_table_association" "crt_public_subnet" {
 }
 
 # --
+resource "aws_security_group" "sg_public_instance" {
+  name        = "Public instance SG"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc_main.id
 
+  tags = {
+    Name = "Allow SSH and anyevery"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.sg_public_instance.id
+  description       = "SSH over internet"
+  cidr_ipv4         = aws_vpc.vpc_main.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_public_instance.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
