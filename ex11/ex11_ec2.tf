9576c116c8e8906d1cff37412b5ed39e65e654ad
ernesto diaz <ernesto.diaz@miliopay.com>
2024-12-30 15:43:12 -0500

    :sparkles: usage new command for run code in instance

--- ex11/ex11_ec2.tf
+++ ex11/ex11_ec2.tf
@@ -8,21 +8,27 @@ resource "aws_instance" "public_instance" {
     #!/bin/bash
     # Actualiza los paquetes
     yum update -y
-    # Instala Node.js
-    curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-    yum install -y nodejs
+    # Instala curl y nvm (Node Version Manager)
+    yum install -y curl
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh" # Carga nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion" # Carga autocompletado
+    # Instala la última versión de Node.js
+    nvm install --lts
     # Crea una aplicación simple en Node.js
     echo "const http = require('http');
     const server = http.createServer((req, res) => {
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.end('<h1>Hello world!!</h1>');
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World\\n');
     });
     server.listen(80, () => {
       console.log('Server running on port 80');
     });" > /home/ec2-user/app.js
     # Ejecuta la aplicación al iniciar la máquina
     nohup node /home/ec2-user/app.js &
+    echo 'hello world' > ~/greeting.txt
   EOF
   provisioner "local-exec" {
     command = "echo 'Hello from ${aws_instance.public_instance.public_ip}' >> instance_created.txt"
@@ -32,15 +38,15 @@ resource "aws_instance" "public_instance" {
     command = "echo 'Bye bye from ${self.public_ip}' >> instance_distroyed.txt"
   }
 
-  provisioner "remote-exec" {
-    inline = ["echo 'hello world' > ~/greeting.txt"]
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("myKey.pem")
-    }
-  }
+  # provisioner "remote-exec" {
+  #   inline = ["echo 'hello world' > ~/greeting.txt"]
+  #   connection {
+  #     type = "ssh"
+  #     host = self.public_ip
+  #     user = "ec2-user"
+  #     private_key = file("myKey.pem")
+  #   }
+  # }
   tags = {
     Name : "Web Server 1"
   }
