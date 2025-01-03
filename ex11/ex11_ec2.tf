d7e210de3a929bc0bcd029df5fe6ca7b3bcad809
ernesto diaz <ernesto.diaz@miliopay.com>
2025-01-03 17:59:59 -0500

    :sparkles: temporal code imported

--- ex11/ex11_ec2.tf
+++ ex11/ex11_ec2.tf
@@ -19,3 +19,15 @@ resource "aws_instance" "public_instance" {
   }
 }
 */
+
+resource "aws_instance" "Web_Server" {
+  ami           = "ami-01816d07b1128cd2d"
+  instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.myKey.key_name
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    "Name" = "Web Server"
+  }
+  vpc_security_group_ids      = [aws_security_group.sg_public_instance.id]
+  user_data_replace_on_change = true
+}
