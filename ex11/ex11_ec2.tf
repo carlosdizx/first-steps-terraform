5c0aa720383ef2e1a854a8e934defb1f57004c88
ernesto diaz <ernesto.diaz@miliopay.com>
2025-01-03 16:58:58 -0500

    :sparkles: desacoplate logic from user data

--- ex11/ex11_ec2.tf
+++ ex11/ex11_ec2.tf
@@ -4,28 +4,7 @@ resource "aws_instance" "public_instance" {
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.myKey.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
-  #user_data = file("scripts/ec2_user_data.sh")
-  user_data = <<-EOF
-    #!/bin/bash
-    # Actualiza los paquetes
-    yum update -y
-    # Instala Node.js
-    curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-    yum install -y nodejs
-    # Crea una aplicación simple en Node.js
-    # shellcheck disable=SC2028
-    echo "const http = require('http');
-    const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Hello World\\n');
-    });
-    server.listen(80, () => {
-      console.log('Server running on port 80');
-    });" > /home/ec2-user/app.js
-    # Ejecuta la aplicación al iniciar la máquina
-    nohup node /home/ec2-user/app.js &
-  EOF
+  user_data              = file("scripts/ec2_user_data.sh")
   provisioner "local-exec" {
     command = "echo 'Hello from ${aws_instance.public_instance.public_ip}' >> instance_created.txt"
   }
--- ex11/scripts/ec2_user_data.sh
+++ ex11/scripts/ec2_user_data.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 # Actualiza los paquetes
 yum update -y
-# Instala curl y nvm (Node Version Manager)
-yum install -y curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh" # Carga nvm
-[ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion" # Carga autocompletado
-# Instala la última versión de Node.js
-nvm install --lts
+# Instala Node.js
+curl -sL https://rpm.nodesource.com/setup_22.x | bash -
+yum install -y nodejs
 # Crea una aplicación simple en Node.js
 # shellcheck disable=SC2028
 echo "const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello world!</h1>');
 });
 server.listen(80, () => {
   console.log('Server running on port 80');
 });" > /home/ec2-user/app.js
 # Ejecuta la aplicación al iniciar la máquina
 nohup node /home/ec2-user/app.js &
-echo 'hello world' > ~/greeting.txt
