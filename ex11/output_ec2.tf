428a608b9caafa2ca7507db67aaac2304254398e
ernesto diaz <ernesto.diaz@miliopay.com>
2025-01-03 16:04:31 -0500

    :sparkles: replace output

--- ex11/output_ec2.tf
+++ ex11/output_ec2.tf
@@ -1,4 +1,4 @@
 output "output_ec2_ip_public" {
   description = "Ip publica de la instancia EC2"
-  value       = aws_instance.public_instance.public_ip
+  value       = "Dirección de la  http://${aws_instance.public_instance.public_ip}"
 }
