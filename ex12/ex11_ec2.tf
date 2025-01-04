774ce3fbe30a52897ad710fc1a6482a837710cee
ernesto diaz <ernesto.diaz@miliopay.com>
2025-01-04 10:16:36 -0500

    :truck: change name script

--- ex12/ex11_ec2.tf
+++ ex12/ex11_ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "instance_base" {
   subnet_id                   = aws_subnet.public_subnet.id
   key_name                    = data.aws_key_pair.myKey.key_name
   vpc_security_group_ids      = [aws_security_group.sg_public_instance.id]
-  user_data                   = file("scripts/ec2_user_data.sh")
+  user_data                   = file("scripts/node_script.sh")
   user_data_replace_on_change = true
   tags = {
     Name : each.value
