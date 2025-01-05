f2fa18bbd570abdf04201fc367851a6faca8027d
ernesto diaz <ernesto.diaz@miliopay.com>
2025-01-05 13:46:08 -0500

    :hammer: create script for run server for monitoring instances

---
+++ ex12/scripts/monitoring_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum update -y
+yum install -y curl
+# shellcheck disable=SC2154
+for ip in ${join(" ", aws_instance.servers[*].public_ip)}; do
+  echo "Checking server at $ip..." > /home/ec2-user/"${ip}.txt"
+  curl -s "http://$ip" || echo "Server at $ip is DOWN"
+done
