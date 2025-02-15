fa5682051d4fd966356e752a5a639fc74e900f72
ernesto diaz <ernesto.diaz@miliopay.com>
2025-02-14 23:02:47 -0500

    :sparkles: modify configure

--- ex12/ex11_ec2.tf
+++ ex12/ex11_ec2.tf
@@ -12,21 +12,3 @@ resource "aws_instance" "servers" {
   }
 }
 
-resource "aws_instance" "checker_instance" {
-  count                  = var.enable_checker_instance ? 1 : 0
-  ami                    = var.details_instances.ami
-  instance_type          = var.details_instances.type
-  subnet_id              = aws_subnet.public_subnet.id
-  key_name               = data.aws_key_pair.myKey.key_name
-  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
-  user_data              = file("scripts/monitoring_script.sh")
-  tags = {
-    Name = "Checker Instance"
-  }
-
-  provisioner "local-exec" {
-    when    = create
-    command = "echo 'Checking servers...' > app.js"
-  }
-}
-
