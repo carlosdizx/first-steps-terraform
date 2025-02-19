445cd2c8639d8db15015c4f60e1616bf9c75d9b6
ernesto diaz <ernesto.diaz@miliopay.com>
2025-02-18 20:44:43 -0500

    :sparkles: add new inbound rule

--- ex13/ex13_vpc.tf
+++ ex13/ex13_vpc.tf
@@ -81,6 +81,14 @@ resource "aws_security_group" "sg_public_instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Streamlit over internet"
+    from_port   = 8503
+    to_port     = 8503
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "HTTP over internet"
     from_port   = 0
